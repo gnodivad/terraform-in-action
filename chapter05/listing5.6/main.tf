830c89509b7404f307aad4a13c42ac1556978e63
David Ong <david.ong@grabtaxi.com>
2021-11-28 23:27:05 +0800

    Finish Chapter 05

---
+++ chapter05/listing5.6/main.tf
@@ -0,0 +1,28 @@
+resource "random_string" "rand" {
+  length  = 24
+  special = false
+  upper   = false
+}
+
+locals {
+  namespace = substr(join("-", [var.namespace, random_string.rand.result]), 0, 24)
+}
+
+resource "azurerm_resource_group" "default" {
+  name     = local.namespace
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = random_string.rand.result
+  resource_group_name      = azurerm_resource_group.default.name
+  location                 = azurerm_resource_group.default.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "storage_container" {
+  name                  = "serverless"
+  storage_account_name  = azurerm_storage_account.storage_account.name
+  container_access_type = "private"
+}
---
+++ chapter05/listing5.8/main.tf
@@ -0,0 +1,71 @@
+resource "random_string" "rand" {
+  length  = 24
+  special = false
+  upper   = false
+}
+
+locals {
+  namespace = substr(join("-", [var.namespace, random_string.rand.result]), 0, 24)
+}
+
+resource "azurerm_resource_group" "default" {
+  name     = local.namespace
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = random_string.rand.result
+  resource_group_name      = azurerm_resource_group.default.name
+  location                 = azurerm_resource_group.default.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "storage_container" {
+  name                  = "serverless"
+  storage_account_name  = azurerm_storage_account.storage_account.name
+  container_access_type = "private"
+}
+
+module "ballroom" {
+  source = "terraform-in-action/ballroom/azure"
+}
+
+resource "azurerm_storage_blob" "storage_blob" {
+  name                   = "server.zip"
+  storage_account_name   = azurerm_storage_account.storage_account.name
+  storage_container_name = azurerm_storage_container.storage_container.name
+  type                   = "Block"
+  source                 = module.ballroom.output_path
+}
+
+data "azurerm_storage_account_sas" "storage_sas" {
+  connection_string = azurerm_storage_account.storage_account.primary_connection_string
+
+  resource_types {
+    service   = false
+    container = false
+    object    = true
+  }
+
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+
+  start  = "2016-06-19T00:00:00Z"
+  expiry = "2048-06-19T00:00:00Z"
+
+  permissions {
+    read    = true
+    write   = false
+    delete  = false
+    list    = false
+    add     = false
+    create  = false
+    update  = false
+    process = false
+  }
+}
