d575fe5b234636fe2498d4c66623a5548d0d6216
David Ong <david.ong@grabtaxi.com>
2021-12-02 23:22:41 +0800

    Finish listing7.7

---
+++ chapter07/listing7.5/main.tf
@@ -0,0 +1,31 @@
+locals {
+  services = [
+    "sourcerepo.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "run.googleapis.com",
+    "iam.googleapis.com",
+  ]
+}
+
+resource "google_project_service" "enabled_service" {
+  for_each = toset(local.services)
+  project  = var.project_id
+  service  = each.key
+
+  provisioner "local-exec" {
+    command = "sleep 60"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "sleep 15"
+  }
+}
+
+resource "google_sourcerepo_repository" "repo" {
+  depends_on = [
+    google_project_service.enabled_service["sourcerepo.googleapis.com"]
+  ]
+
+  name = "${var.namespace}-repo"
+}
---
+++ chapter07/listing7.6/main.tf
@@ -0,0 +1,76 @@
+locals {
+  services = [
+    "sourcerepo.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "run.googleapis.com",
+    "iam.googleapis.com",
+  ]
+}
+
+resource "google_project_service" "enabled_service" {
+  for_each = toset(local.services)
+  project  = var.project_id
+  service  = each.key
+
+  provisioner "local-exec" {
+    command = "sleep 60"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "sleep 15"
+  }
+}
+
+resource "google_sourcerepo_repository" "repo" {
+  depends_on = [
+    google_project_service.enabled_service["sourcerepo.googleapis.com"]
+  ]
+
+  name = "${var.namespace}-repo"
+}
+
+locals {
+  image = "gcr.io/${var.project_id}/${var.namespace}"
+  steps = [
+    {
+      name = "gcr.io/cloud-builders/go"
+      args = ["test"]
+      env  = ["PROJECT_ROOT=${var.namespace}"]
+    },
+    {
+      name = "gcr.io/cloud-builders/docker"
+      args = ["build", "-t", local.image, "."]
+    },
+    {
+      name = "gcr.io/cloud-builders/docker"
+      args = ["push", local.image]
+    },
+    {
+      name = "gcr.io/cloud-builders/gcloud"
+      args = ["run", "deploy", google_cloud_run_service.service.name, "--image", local.image, "--region", var.region, "--platform", "managed", "-q"]
+    }
+  ]
+}
+
+resource "google_cloudbuild_trigger" "trigger" {
+  depends_on = [
+    google_project_service.enabled_service["cloudbuild.googleapis.com"]
+  ]
+
+  trigger_template {
+    branch_name = "master"
+    repo_name   = google_sourcerepo_repository.repo.name
+  }
+
+  build {
+    dynamic "step" {
+      for_each = local.steps
+      content {
+        name = step.value.name
+        args = step.value.args
+        env  = lookup(step.value, "env", null)
+      }
+    }
+  }
+}
